7bfd45205839eb751cdbdeff564d8213aa2a357e
MrWalshyType2 <morgan.walsh@academytrainee.com>
2020-07-01 17:12:52 +0100

    Filled sql-schema file to match ERD

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -1,2 +1,26 @@
 create database if not exists ims;
-create table if not exists ims.customers(id int primary key auto_increment, first_name varchar(40), surname varchar(40));
+
+create table if not exists ims.customers(
+	id int primary key auto_increment not null unique,
+    first_name varchar(32) not null, 
+    surname varchar(32) not null,
+    email varchar(64) not null,
+    username varchar(16) not null unique,
+    password varchar(16) not null
+);
+
+create table if not exists ims.orders(
+	id int primary key auto_increment not null unique,
+    fk_customer_id int not null,
+    fk_item_id int not null,
+    customer varchar(32) not null,
+    foreign key (fk_customer_id) references customers(id),
+    foreign key (fk_item_id) references items(id)
+);
+
+create table if not exists ims.items(
+	id int primary key auto_increment not null unique,
+    item_name varchar(32) not null,
+    value int not null,
+    amount int not null
+);
