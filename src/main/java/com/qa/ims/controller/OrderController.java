3382f45ade1ccc628333348570b77c42fdafd0d6
MrWalshyType2 <morgan.walsh@academytrainee.com>
2020-07-10 15:08:37 +0100

    IW5-48 #comment Created readAll() and create() #time 10m

--- src/main/java/com/qa/ims/controller/OrderController.java
+++ src/main/java/com/qa/ims/controller/OrderController.java
@@ -2,20 +2,43 @@ package com.qa.ims.controller;
 
 import java.util.List;
 
+import org.apache.log4j.Logger;
+
 import com.qa.ims.persistence.domain.Order;
+import com.qa.ims.services.CrudServices;
+import com.qa.ims.utils.Utils;
 
 public class OrderController implements CrudController<Order> {
 
+	public static final Logger LOGGER = Logger.getLogger(OrderController.class);
+
+	private CrudServices<Order> orderService;
+
+	public OrderController(CrudServices<Order> orderService) {
+		this.orderService = orderService;
+	}
+
+	String getInput() {
+		return Utils.getInput();
+	}
+
 	@Override
 	public List<Order> readAll() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Order> orders = orderService.readAll();
+		for (Order order : orders) {
+			LOGGER.info(order.toString());
+		}
+		return orders;
 	}
 
 	@Override
 	public Order create() {
-		// TODO Auto-generated method stub
-		return null;
+		LOGGER.info("Please enter your customer id: ");
+		long customerId = Long.getLong(getInput());
+
+		Order order = new Order(customerId);
+		LOGGER.info("Order created!");
+		return order;
 	}
 
 	@Override
