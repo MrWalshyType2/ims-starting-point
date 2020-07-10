43078061d9325ff1c8c144eb9bb36b150c10a7a4
MrWalshyType2 <morgan.walsh@academytrainee.com>
2020-07-10 14:23:43 +0100

    IW5-47 #comment Created OrdersDao #in-progress #time 5m

---
+++ src/main/java/com/qa/ims/persistence/dao/OrderDaoMysql.java
@@ -0,0 +1,52 @@
+package com.qa.ims.persistence.dao;
+
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+import com.qa.ims.persistence.domain.Order;
+
+public class OrderDaoMysql implements Dao<Order> {
+
+	public static final Logger LOGGER = Logger.getLogger(OrderDaoMysql.class);
+
+	private String jdbcConnectionUrl;
+	private String username;
+	private String password;
+
+	public OrderDaoMysql(String username, String password) {
+		this.username = username;
+		this.password = password;
+	}
+
+	public OrderDaoMysql(String jdbcConnectionUrl, String username, String password) {
+		this.jdbcConnectionUrl = jdbcConnectionUrl;
+		this.username = username;
+		this.password = password;
+	}
+
+	@Override
+	public List<Order> readAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Order create(Order t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Order update(Order t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(long id) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
