ba7a7f245e181c8be938f685f0d05db4698f499e
MrWalshyType2 <morgan.walsh@academytrainee.com>
2020-07-10 12:48:53 +0100

    IW5-45 #comment Added ItemController, added getInput() and readAll() #in-progress #time 5m

---
+++ src/main/java/com/qa/ims/controller/ItemController.java
@@ -0,0 +1,52 @@
+package com.qa.ims.controller;
+
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+import com.qa.ims.persistence.domain.Item;
+import com.qa.ims.services.CrudServices;
+import com.qa.ims.utils.Utils;
+
+public class ItemController implements CrudController<Item> {
+
+	public static final Logger LOGGER = Logger.getLogger(ItemController.class);
+
+	private CrudServices<Item> itemService;
+
+	public ItemController(CrudServices<Item> itemService) {
+		this.itemService = itemService;
+	}
+
+	String getInput() {
+		return Utils.getInput();
+	}
+
+	@Override
+	public List<Item> readAll() {
+		List<Item> items = itemService.readAll();
+		for (Item item : items) {
+			LOGGER.info(item.toString());
+		}
+		return items;
+	}
+
+	@Override
+	public Item create() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Item update() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
