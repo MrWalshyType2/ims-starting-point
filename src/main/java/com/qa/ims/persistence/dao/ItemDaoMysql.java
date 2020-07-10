ffbd0e3db9ef3894d06e45c7fd2b4b01eed1bdef
MrWalshyType2 <morgan.walsh@academytrainee.com>
2020-07-10 12:06:02 +0100

    IW5-44 #comment Created ItemDao, connection methods #time 15m #doing

---
+++ src/main/java/com/qa/ims/persistence/dao/ItemDaoMysql.java
@@ -0,0 +1,64 @@
+package com.qa.ims.persistence.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+import com.qa.ims.persistence.domain.Item;
+import com.qa.ims.utils.Utils;
+
+public class ItemDaoMysql implements Dao<Item> {
+
+	public static final Logger LOGGER = Logger.getLogger(ItemDaoMysql.class);
+
+	private String jdbcConnectionUrl;
+	private String username;
+	private String password;
+
+	public ItemDaoMysql(String username, String password) {
+		this.jdbcConnectionUrl = "jdbc:mysql://" + Utils.MYSQL_URL + "/ims";
+		this.username = username;
+		this.password = password;
+	}
+
+	public ItemDaoMysql(String jdbcConnectionUrl, String username, String password) {
+		this.jdbcConnectionUrl = jdbcConnectionUrl;
+		this.username = username;
+		this.password = password;
+	}
+
+	Item itemFromResultSet(ResultSet resultSet) throws SQLException {
+		Long id = resultSet.getLong("id");
+		String itemName = resultSet.getString("itemId");
+		int value = resultSet.getInt("value");
+		int amount = resultSet.getInt("amount");
+		return new Item(id, itemName, value, amount);
+	}
+
+	@Override
+	public List<Item> readAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Item create(Item t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Item update(Item t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(long id) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
