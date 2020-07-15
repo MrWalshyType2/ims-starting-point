93577bc60f410fa9dfdff534b08360a30afe347f
MrWalshyType2 <morgan.walsh@academytrainee.com>
2020-07-15 08:44:04 +0100

    IW5-56 #comment Created getInputTest() #time 2m

--- src/test/java/com/qa/ims/controller/OrderControllerTest.java
+++ src/test/java/com/qa/ims/controller/OrderControllerTest.java
@@ -1,5 +1,11 @@
 package com.qa.ims.controller;
 
+import static org.junit.Assert.assertEquals;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -21,4 +27,16 @@ public class OrderControllerTest {
 	@InjectMocks
 	private OrderController orderController;
 
+	@Test
+	public void getInputTest() {
+		String input = "TheOrderController";
+		InputStream in = new ByteArrayInputStream(input.getBytes()); // Gets an array of bytes representing input
+		System.setIn(in); // Sets the system input stream to the Byte array stream
+		assertEquals("TheOrderController", orderController.getInput());
+	}
+
+	@Test
+	public void readAllTest() {
+
+	}
 }
