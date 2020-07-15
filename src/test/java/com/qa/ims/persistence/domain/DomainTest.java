cc52e621e465b70d048c0d0be972e42aa9f5682d
MrWalshyType2 <morgan.walsh@academytrainee.com>
2020-07-15 12:18:51 +0100

    IW5-58 #in-progress #time 10m #comment Started, unsure how to test printDomains and getDomain

--- src/test/java/com/qa/ims/persistence/domain/DomainTest.java
+++ src/test/java/com/qa/ims/persistence/domain/DomainTest.java
@@ -5,29 +5,34 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 public class DomainTest {
-	
+
 	@Test
 	public void customerTest() {
 		Domain domain = Domain.CUSTOMER;
 		assertTrue(domain.getDescription().toLowerCase().contains("customer"));
 	}
-	
+
 	@Test
 	public void itemTest() {
 		Domain domain = Domain.ITEM;
 		assertTrue(domain.getDescription().toLowerCase().contains("item"));
 	}
-	
+
 	@Test
 	public void orderTest() {
 		Domain domain = Domain.ORDER;
 		assertTrue(domain.getDescription().toLowerCase().contains("items"));
 	}
-	
+
 	@Test
 	public void stopTest() {
 		Domain domain = Domain.STOP;
 		assertTrue(domain.getDescription().toLowerCase().contains("close"));
 	}
+
+//	@Test
+//	public static void printDomainsTest() {
+//
+//	}
 
 }
